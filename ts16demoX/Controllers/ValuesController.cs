934eb630e26e636844c9bda7fa04d01d2ce89166
Daniel Meixner <daniel.meixner@live.com>
2016-11-30 14:18:03 +0100

    quotes

--- ts16demoX/Controllers/ValuesController.cs
+++ ts16demoX/Controllers/ValuesController.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ts16demoX.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public ValuesController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            // default
+            // return new string[] { "value1", "value2" };
+
+            //works locally            
+            var lines = System.IO.File.ReadAllLines("wwwroot/quotes.txt");
+
+            // works everywhere
+            //string webRootPath = hostingEnvironment.WebRootPath;
+            //var lines = System.IO.File.ReadAllLines(webRootPath + @"/quotes.txt");
+            return lines;
         }
 
         // GET api/values/5
